d35561ad8a1b465980abf77410b4708d52413b5e
Diamo-B <diamobachar06@gmail.com>
2022-10-10 13:45:23 +0100

    Cleaned the code a bit and added a pointer cursor effect while hovering on the 'li', 'button', 'select' tags and the logo

--- jquery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {  
-    var objectHeight = 0;
-    $.each($('.hero').children(),function(){
-        objectHeight += $(this).height();
-    });
-    $('.hero').height(objectHeight);
-})
